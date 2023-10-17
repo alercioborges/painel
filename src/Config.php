b067f9a8967ac5cc6a7707a895c1a835562cbbc0
Alercio Borges <alercioborges@hotmail.com>
2023-10-17 17:25:40 -0300

    17-10-2023

--- src/Config.php
+++ src/Config.php
@@ -6,7 +6,7 @@ class Config {
     
     const BASE_DIR = "/painel";
 
-    const WWWROOT = "http://localhost/painel/";
+    const BASE_URL = "http://localhost/painel";
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
